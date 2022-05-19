aa8bce1c1f83b3eaf39d02916fde0a4755be62f0
my9k <93728194+my9k@users.noreply.github.com>
2022-05-19 22:46:38 +0530

    Update SpringMvcApplicationTests.java

--- src/test/java/com/springmvc/SpringMvcApplicationTests.java
+++ src/test/java/com/springmvc/SpringMvcApplicationTests.java
@@ -1,6 +1,9 @@
 package com.springmvc;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -13,52 +16,86 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
 
 import com.springmvc.entity.Customer;
 import com.springmvc.repo.CustomerRepo;
-import com.springmvc.service.impl.CustomerServiceImpl;
+
+import junit.framework.Assert;
+//import com.springmvc.service.impl.CustomerServiceImpl;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+@SuppressWarnings("deprecation")
+@DataJpaTest
 class SpringMvcApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
-	@MockBean
+	@Autowired
 	private CustomerRepo repository;
 	
-	@Autowired
-	private CustomerServiceImpl service;
+//	@Autowired
+//	private CustomerServiceImpl service;
 	
-	@SuppressWarnings("unchecked")
+//	@SuppressWarnings("unchecked")
+//	@Test
+//	public void getCutomersTest() {
+//		when(repository.findAll()).thenReturn(Stream.of(new Customer("Mayank","838403098","mayank@gmail.com"),new Customer("lakhan","8457490580","anshika@gmail.com")).collect(Collectors.toList()));
+//		//int size = service.size();
+//		
+//     	int size = 2;
+//		assertEquals(2,size);
+//	}
+	
 	@Test
-	public void getCutomersTest() {
-		when(repository.findAll()).thenReturn(Stream.of(new Customer("Mayank","838403098","mayank@gmail.com"),new Customer("lakhan","8457490580","anshika@gmail.com")).collect(Collectors.toList()));
-		//int size = service.size();
-		
-     	int size = 2;
-		assertEquals(2,size);
+	
+	public void testCreateCustomer() {
+		Long id = 1l;
+		Customer cust = new Customer("Anjum","85094580","Anjum@gmail.com");
+		Customer customer = repository.save(cust);
+		assertNotNull(customer);
+	}
+	
+	@Test
+	public void testFindCustomerById() {
+		Long id = 1l;
+		Customer cust = repository.getById(id);
+		assertEquals(cust.getCustID(),id);
 	}
 	
 //	@Test
-//	public void addCustomerTest() {
-//		Customer cust = new Customer("Mayank","093830934830","may9kWgmial.com");
-//		when(repository.save(cust)).thenReturn(cust);
-//		assertEquals(cust, service.addCustomer(cust));
+//	@Rollback(false)
+//	public void testDeleteProduct() {
+//		Long id = 1l;
+//		boolean isExistBeforeDelete = repository.findById(id).isPresent();
+//		repository.deleteById(id);
+//		
+//		boolean notExistAfterDelete = repository.findById(id).isPresent();
+//		
+//		assertTrue(isExistBeforeDelete);
+//		assertTrue(notExistAfterDelete);
 //	}
 	
 //	@Test
-//	public void deleteTest() {
-//		Customer cust = new Customer("Mayank","8343094","abah@gmail.com");
-//		service.delete(8l);
-//		verify(repository,times(1)).deleteById(8l);
+//	
+//	public void testListCustomer() {
+//		List<Customer> customer = (List<Customer>)repository.findAll();
+//		for(Customer cust: customer) {
+//			System.out.println(cust);
+//		}
 //		
+//		//Assert.assertThat(customer).size().isGreaterThan(0);
+//		int size = customer.size();
+//		assertTrue(size> 3);
 //	}
+
 }
 
